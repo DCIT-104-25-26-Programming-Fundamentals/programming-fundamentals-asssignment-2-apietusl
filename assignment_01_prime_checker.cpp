5249a2da4689c8d59c76e9237e1eb9613f57cd0e
apietusl <richardapietu121@gmail.com>
2026-08-02 21:15:34 +0000

    Update assignment_01_prime_checker.cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,4 +36,31 @@
 
 #include <iostream>
 using namespace std;
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;          // 2 and 3 are prime
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
 
+    // Check divisors of the form 6k ± 1 up to sqrt(n)
+    for (int i = 5; (long long)i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a prime number." << endl;
+    else
+        cout << num << " is not a prime number." << endl;
+
+    return 0;
+}
